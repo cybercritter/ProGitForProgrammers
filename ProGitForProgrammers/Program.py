e63f8473eb3e2199800979242e1619ecd755e6b2
Michael Reid <pershing2.crr@gmail.com>
2021-10-31 17:19:30 -0600

    Add print indicating we are in the command line

--- ProGitForProgrammers/Program.py
+++ ProGitForProgrammers/Program.py
@@ -1,7 +1,8 @@
 class Program:
     def __init__(self):
-        print("Hello World!")
+        print('Hello World!')
         print('I just added this in PyCharm')
+        print('I just added this to the command line repo')
 
 
 if __name__ == '__main__':
