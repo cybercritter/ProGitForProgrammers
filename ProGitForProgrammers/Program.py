a9ad792964424a74bab8e465c8c6795adc1d1cef
Michael Reid <pershing2.crr@gmail.com>
2021-10-31 17:14:30 -0600

    Add informative line

--- ProGitForProgrammers/Program.py
+++ ProGitForProgrammers/Program.py
@@ -1,8 +1,9 @@
 class Program:
     def __init__(self):
         print("Hello World!")
+        print('I just added this in PyCharm')
 
 
 if __name__ == '__main__':
     program = Program()
-    
+
